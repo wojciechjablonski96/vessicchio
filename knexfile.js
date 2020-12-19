96de4510f3b25eb80e9b8b8d50478a53f0d29e36
Wojciech Jablonski <info@wojciechjablonski.com>
2020-12-19 20:30:55 +0100

    Fixed code style

--- knexfile.js
+++ knexfile.js
@@ -3,43 +3,43 @@ dotenv.config();
 
 module.exports = {
 
-  development: {
-    client: 'mysql2',
-    connection: {
-      host : process.env.MYSQL_HOST,
-      user : process.env.MYSQL_USER,
-      password : process.env.MYSQL_DB,
-      database : process.env.MYSQL_PASSWORD
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_DB,
+            database: process.env.MYSQL_PASSWORD
+        },
+        migrations: {
+            tableName: 'migrations'
+        }
     },
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
 
-  staging: {
-    client: 'mysql2',
-    connection: {
-      host : process.env.MYSQL_HOST,
-      user : process.env.MYSQL_USER,
-      password : process.env.MYSQL_DB,
-      database : process.env.MYSQL_PASSWORD
+    staging: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_DB,
+            database: process.env.MYSQL_PASSWORD
+        },
+        migrations: {
+            tableName: 'migrations'
+        }
     },
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
 
-  production: {
-    client: 'mysql2',
-    connection: {
-      host : process.env.MYSQL_HOST,
-      user : process.env.MYSQL_USER,
-      password : process.env.MYSQL_DB,
-      database : process.env.MYSQL_PASSWORD
-    },
-    migrations: {
-      tableName: 'migrations'
+    production: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_DB,
+            database: process.env.MYSQL_PASSWORD
+        },
+        migrations: {
+            tableName: 'migrations'
+        }
     }
-  }
 
 };
