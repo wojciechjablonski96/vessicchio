19c197fa49963ab1b3d4627902d6062a96acdc95
Wojciech Jablonski <info@wojciechjablonski.com>
2020-12-19 20:21:27 +0100

    created create guilds table

--- migrations/20201006150123_create_guild_table.js
+++ migrations/20201006150123_create_guild_table.js
@@ -1,16 +1,15 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('guilds', table => {
-            table.string('id').notNullable().primary()
-            table.string('config_channel').nullable()
-            table.string('channel_log').nullable()
-            table.string('owner').nullable()
-            table.text('administrators').nullable()
-            table.text('users').nullable()
-            table.string('locale').nullable()
-            table.timestamp('timestamp').defaultTo(knex.fn.now())
+        table.string('id').notNullable().primary()
+        table.string('owner').nullable()
+        table.text('administrators').nullable()
+        table.text('users').nullable()
+        table.string('locale').nullable()
+        table.string('prefix').nullable()
+        table.timestamp('timestamp').defaultTo(knex.fn.now())
     })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('guilds');
 };
