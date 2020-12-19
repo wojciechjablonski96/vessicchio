427047e542094429ba83f51b8a7c8ca2094ba53c
Wojciech Jablonski <info@wojciechjablonski.com>
2020-12-19 20:21:44 +0100

    Created create playlist table

---
+++ migrations/20201218091410_create_playlist_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema
+            .createTable('playlist', table => {
+                table.increments('id')
+                    .primary()
+                table.string('guild')
+                table.string('channel')
+                table.string('added_by')
+                table.text('title')
+                table.text('url')
+                table.timestamp('added')
+                    .defaultTo(knex.fn.now())
+                table.timestamp('played')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('playlist');
+};
