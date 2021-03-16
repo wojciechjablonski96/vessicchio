377c96a93205b5e4b172c3dd5a4e7185d6f941c7
Wojciech Jablonski <info@wojciechjablonski.com>
2021-03-16 23:01:43 +0100

    Created Guild Class

--- app/Guild.js
+++ app/Guild.js
@@ -1,145 +1,40 @@
-const Discord = require('discord.js');
-
-const dbconnection = require("../inc/db.js");
+const database = require('../inc/db');
 
 class Guild {
-
     constructor(guild) {
-        this.id = guild.id;
-        this.isNewGuild().then(result => {
-            if (result)
-                this.loadOptions();
-            else
-                this.createOptions(guild);
-        })
+        this.guild = guild;
     }
 
-    createOptions(guild) {
-        this.channel_log = null;
-        this.owner = guild.ownerID;
-        this.administrators = [];
-        this.users = [];
-        this.locale = guild.preferredLocale;
-        this.createconfigurationChannel(guild).then(id => {
-            this.config_channel = id;
-            this.store();
-        });
-    }
-
-    createconfigurationChannel(guild) {
+    getGuild() {
         let that = this;
         return new Promise(function (resolve, reject) {
-            guild.channels.create(`configuration-vessicchio`,
-                {
-                    type: 'text'
-                }).then(async c => {
-                try {
-                    let permissions = [
-                        {
-                            id: c.guild.roles.everyone,
-                            deny: ['VIEW_CHANNEL'],
-                        },
-                        {
-                            id: that.owner,
-                            allow: ['VIEW_CHANNEL']
-                        },
-                    ];
-                    await c.overwritePermissions(permissions).then(
-                        c => {
-                            resolve(c.id);
-                        }
-                    );
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        });
-    }
-
-    loadOptions() {
-        this.get().then(results => {
-            results.forEach(result => {
-                this.channel_log = result.channel_log;
-                this.config_channel = result.config_channel;
-                this.owner = result.owner;
-                this.administrators = result.administrators;
-                this.users = result.users;
-                this.locale = result.locale;
-            });
-        });
-    }
-
-    isNewGuild() {
-        let that = this;
-        return new Promise(function (resolve, reject) {
-            dbconnection('guilds')
-                .count('id as qty')
-                .where('id', '=', that.id)
-                .then(
-                    (rows) => {
-                        if (rows[0]['qty'] >= 1) {
-                            resolve(true)
-                        } else {
-                            resolve(false)
-                        }
-                    })
-                .catch(e => {
-                    reject(e)
-                })
-        })
-    }
-
-    get() {
-        let that = this;
-        return new Promise(function (resolve, reject) {
-            dbconnection('guilds')
-                .where({id: that.id})
+            database('guilds')
+                .where({id: that.guild.id})
                 .then(rows => {
-                    resolve(rows)
-                })
-                .catch(e => {
-                    reject(e)
+                    return resolve(rows[0]);
                 })
         });
     }
 
-    update() {
-        dbconnection('guilds')
-            .where({id: this.id})
-            .update({
-                config_channel: this.config_channel,
-                owner: this.owner,
-                administrators: JSON.stringify(this.administrators),
-                users: JSON.stringify(this.users),
-                locale: this.locale
-            }).catch(e => {
-            console.log('error: ' + e)
-        })
+    newGuild() {
+        database('guilds').insert({
+            id: this.guild.id,
+            owner: this.guild.ownerID,
+            administrators: JSON.stringify(Array()),
+            users: JSON.stringify(Array()),
+            locale: this.guild.preferredLocale,
+            prefix: '!',
+        }).catch(e => {
+            console.log(e);
+        });
     }
 
-    store() {
-        dbconnection('guilds')
-            .insert({
-                id: this.id,
-                config_channel: this.config_channel,
-                owner: this.owner,
-                administrators: JSON.stringify(this.administrators),
-                users: JSON.stringify(this.users),
-                locale: this.locale
-            }).catch(e => {
-            console.log('error: ' + e)
-        })
-    }
-
-    delete() {
-        dbconnection('guilds')
-            .where({id: this.id})
-            .del()
+    deleteGuild() {
+        database('guilds').where({id: this.guild.id}).del()
             .catch(e => {
-                console.log('error: ' + e)
+                console.log(e);
             })
     }
-
 }
 
 module.exports = Guild;
