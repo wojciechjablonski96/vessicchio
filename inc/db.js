71177eea13b2837bd3f0f89ca3d681e1f66a5c2f
Wojciech Jablonski <info@wojciechjablonski.com>
2021-03-16 22:59:32 +0100

    changed database var name

--- inc/db.js
+++ inc/db.js
@@ -1,14 +1,14 @@
-var dbconnection = require('knex')({
+var database = require('knex')({
     client: 'mysql2',
     connection: {
-      host : process.env.MYSQL_HOST,
-      user : process.env.MYSQL_USER,
-      password : process.env.MYSQL_DB,
-      database : process.env.MYSQL_PASSWORD
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_DB,
+        database: process.env.MYSQL_PASSWORD
     },
     migrations: {
         tableName: 'migrations'
-      }
-  });
+    }
+});
 
-  module.exports = dbconnection;
+module.exports = database;
