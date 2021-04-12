4f1cc0844af9984f89f88f1dd435ea93c714860a
Wojciech Jablonski <info@wojciechjablonski.com>
2021-04-12 22:13:16 +0200

    fixed msgs

--- app/Message.js
+++ app/Message.js
@@ -1,7 +1,7 @@
 /* Copyright (C) Wojciech Jablonski - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Wojciech Jablonski <info@wojciechjablonski.com>, 2021
+ * Written by Wojciech Jablonski <info@wojciechjablonski.com>, April 2021
  */
 
 const Discord = require('discord.js');
@@ -18,16 +18,40 @@ class Message {
             this.autodelete = false;
     }
 
-    createError(errorMessage) {
-        return this.entity.send(
-            new Discord.MessageEmbed()
-                .setColor(process.env.COLOR_ERROR)
-                .setTitle(errorMessage)
-                .setAuthor(process.env.NAME)
-                .setThumbnail(process.env.LOGO)
-                .setFooter(process.env.COPY.toString() + Moment().format('YYYY') + ' | '+ process.env.VERSION.toString())
-                .setTimestamp()
-        ).then(message => {
+    createError(errorMessage, details = null) {
+
+        let msg = new Discord.MessageEmbed()
+            .setColor(process.env.COLOR_ERROR)
+            .setTitle(errorMessage)
+            .setAuthor(process.env.NAME)
+            .setThumbnail(process.env.LOGO)
+            .setFooter(process.env.COPY.toString() + Moment().format('YYYY') + ' | ' + process.env.VERSION.toString())
+            .setTimestamp();
+
+        if (details) msg.setDescription(details);
+
+        return this.entity.send(msg).then(message => {
+            if (this.autodelete)
+                var timeout = setTimeout(function () {
+                    message.delete();
+                    clearTimeout(timeout);
+                }, this.autodelete);
+        });
+    }
+
+    createInfo(infoMessage, details = null) {
+
+        let msg = new Discord.MessageEmbed()
+            .setColor(process.env.COLOR_INFO)
+            .setTitle(infoMessage)
+            .setAuthor(process.env.NAME)
+            .setThumbnail(process.env.LOGO)
+            .setFooter(process.env.COPY.toString() + Moment().format('YYYY') + ' | ' + process.env.VERSION.toString())
+            .setTimestamp();
+
+        if (details) msg.setDescription(details);
+
+        return this.entity.send(msg).then(message => {
             if (this.autodelete)
                 var timeout = setTimeout(function () {
                     message.delete();
