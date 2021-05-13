2e98bf01ff4459eaff649ed16e54d0b84e2263a8
Wojciech Jablonski <info@wojciechjablonski.com>
2021-05-13 11:56:02 +0200

    Created loop command

---
+++ commands/loop.js
@@ -0,0 +1,22 @@
+/* Copyright (C) Wojciech Jablonski - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Wojciech Jablonski <info@wojciechjablonski.com>, 2021
+ */
+const Music = require('../app/Music');
+
+module.exports = {
+    cmd: "loop",
+    description: "Loop your queue!",
+    aliases: [
+        "lp"
+    ],
+    permission: 1,
+    args: false,
+    module: "music",
+
+    execute(client, message, args) {
+        new Music(client, message).loop().finally(() => {
+        });
+    }
+}
