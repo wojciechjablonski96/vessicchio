528b63f943237a8103df3e21b91c825a45f5f2be
Wojciech Jablonski <info@wojciechjablonski.com>
2021-05-17 12:54:41 +0200

    Created clearqueue command

---
+++ commands/clearqueue.js
@@ -0,0 +1,23 @@
+/* Copyright (C) Wojciech Jablonski - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Wojciech Jablonski <info@wojciechjablonski.com>, 2021
+ */
+const Music = require('../app/Music');
+
+module.exports = {
+    cmd: "clearqueue",
+    description: "Delete all queue songs.",
+    aliases: [
+        "cq"
+    ],
+    permission: 1,
+    args: false,
+    module: "music",
+
+    execute(client, message, args) {
+
+        let music = new Music(client, message);
+        music.clearQueue().catch(e => console.log('[CLEARQUEUE COMMAND] '+ e));
+    }
+}
