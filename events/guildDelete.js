1cf1314565838695acf7eef5d1031efb05b52c47
Wojciech Jablonski <info@wojciechjablonski.com>
2021-08-11 11:26:35 +0200

    reworked

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,14 +1,22 @@
-/* Copyright (C) Wojciech Jablonski - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Wojciech Jablonski <info@wojciechjablonski.com>, 2021
+/*
+ * Copyright (C) 2021 Wojciech Jablonski All rights reserved.
+ *
+ * This document is the property of Wojciech Jablonski <info@wojciechjablonski.com>.
+ * It is considered confidential and proprietary.
+ *
+ * This document may not be reproduced or transmitted in any form,
+ * in whole or in part, without the express written permission of
+ * Wojciech Jablonski <info@wojciechjablonski.com>.
  */
 
 const Guild = require('../app/Guild');
+module.exports = class {
+    constructor(client) {
+        this.client = client;
+    }
 
-exports.use = async (DiscordClient) => {
-    DiscordClient.on('guildDelete', (guild) => {
+    async create(guild) {
         const oldguild = new Guild(guild);
         oldguild.deleteGuild().catch(e => console.log('GUILD EXIT: ' + e));
-    });
+    }
 }
