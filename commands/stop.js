9f2207bf0bc125ab2008be7847d90b2131a9afc9
Wojciech Jablonski <info@wojciechjablonski.com>
2022-01-29 12:11:29 +0100

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021 Wojciech Jablonski All rights reserved.
+ *
+ * This document is the property of Wojciech Jablonski <info@wojciechjablonski.com>.
+ * It is considered confidential and proprietary.
+ *
+ * This document may not be reproduced or transmitted in any form,
+ * in whole or in part, without the express written permission of
+ * Wojciech Jablonski <info@wojciechjablonski.com>.
+ */
+
+const {SlashCommand} = require('slash-create');
+
+const Message = require('../app/Message');
+
+module.exports = class stopCommand extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'stop',
+            description: 'Stops your queue',
+        });
+        this.filePath = __filename;
+    }
+
+    async run(ctx) {
+
+        const {client} = require('..');
+        const guild = client.guilds.cache.get(ctx.guildID);
+        const member = guild.members.cache.get(ctx.user.id) ?? await guild.members.fetch(ctx.user.id);
+
+        await ctx.defer();
+
+        if (!member.voice.channel) return ctx.sendFollowUp({
+            embeds: [
+                new Message().createError("You are not in a voice channel!")
+            ], ephemeral: true
+        })
+
+        const queue = client.player.getQueue(ctx.guildID);
+        if (!queue || !queue.playing) return ctx.sendFollowUp({
+            embeds: [
+                new Message().createError("This bot is not playing right now!")
+            ], ephemeral: true
+        });
+        queue.destroy();
+        return ctx.sendFollowUp({
+            embeds: [
+                new Message().createInfo("Bot has been stopped!")
+            ], ephemeral: true
+        });
+    }
+}
+
